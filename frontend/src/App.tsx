f3d82fb089e9fa81cdc299067af009987937abef
SARA EL GHAYATI <saraelghayati726@gmail.com>
2026-07-14 02:28:36 +0100

    feat: wire up multi-page routing with sidebar layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import VehicleDetail from "./pages/VehicleDetail";
+import AssistantPage from "./pages/AssistantPage";
+import Alerts from "./pages/Alerts";
+import Analytics from "./pages/Analytics";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/vehicles/:id" element={<VehicleDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/vehicles/:id" element={<VehicleDetail />} />
+          <Route path="/assistant" element={<AssistantPage />} />
+          <Route path="/alerts" element={<Alerts />} />
+          <Route path="/analytics" element={<Analytics />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default App;
